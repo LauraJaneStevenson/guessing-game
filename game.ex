2d7018afd044f3b2a49f6c77a07c37e1c4555f64
laura stevenson <laurastevenson@lauras-MacBook-Pro.local>
2020-01-03 10:53:27 -0800

    added recursion

---
+++ game.ex
@@ -0,0 +1,36 @@
+defmodule Game do
+  def play do
+
+    hints = "veggie, orange, crunchy \n"
+    IO.puts "Hints: #{hints}"
+
+    # IO.gets prompts user and binds input to variable
+    guess = String.strip(IO.gets "Guess word")
+
+    # call function
+    attempt(guess)
+
+  end
+
+  # break if user guesses correct answer
+  def attempt("carrot") do
+
+    # print you won
+    IO.puts "You Won!"
+
+  end
+
+  def attempt(wrong-answer) do
+
+    # print wrong
+    IO.puts "Wrong!"
+
+    # promt user to guess again and bind input to variable guess
+    guess = String.strip(IO.gets "Guess word")
+
+    # recurse
+    attempt(guess)
+
+  end
+
+end
